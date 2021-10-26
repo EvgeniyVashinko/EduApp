897e89332daf31860c38991de527a2576310e19b
Makar Pundzis <makar.pundzis@itechart-group.com>
2021-10-26 21:33:52 +0300

    entities created

---
+++ EduApp/EduApp.Core/Entities/Order.cs
@@ -0,0 +1,20 @@
+﻿using EduApp.Core.Common;
+using System;
+
+namespace EduApp.Core.Entities
+{
+    public class Order : BaseEntity
+    {
+        public decimal Sum { get; set; }
+
+        public DateTime CreationalDate { get; set; }
+
+        public Guid AccountId { get; set; }
+        
+        public Account Account { get; set; }
+
+        public Guid CourseId { get; set; }
+
+        public Course Course { get; set; }
+    }
+}
---
+++ EduApp/EduApp.Core/Entities/Role.cs
@@ -0,0 +1,12 @@
+﻿using EduApp.Core.Common;
+using System.Collections.Generic;
+
+namespace EduApp.Core.Entities
+{
+    public class Role : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public ICollection<Account> Accounts { get; set; }
+    }
+}
