ea203602b48d5d802828a3e296b86f3f610015bf
EvgeniyVashinko <e.vashinko@gmail.com>
2021-11-26 16:23:24 +0300

    added classes for pagination

---
+++ EduApp/EduApp.Core/Pagination/PagedList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace EduApp.Core.Pagination
+{
+    public class PagedList<T> : PagedListBase
+    {
+        public IReadOnlyList<T> Items { get; }
+
+        public int From => (Page - 1) * PerPage + 1;
+        public int To => From + Items.Count - 1;
+
+        public PagedList(IList<T> items, int totalItems, PageInfo pageInfo) : base(totalItems, pageInfo)
+        {
+            if (items is null)
+            {
+                throw new ArgumentNullException(nameof(items));
+            }
+
+            Items = new ReadOnlyCollection<T>(items);
+        }
+    }
+}
---
+++ EduApp/EduApp.Core/Pagination/PagedListBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EduApp.Core.Pagination
+{
+    public class PagedListBase
+    {
+        public int TotalItems { get; set; }
+        public int Page { get; set; }
+        public int PerPage { get; set; }
+
+        public int TotalPages => TotalItems % PerPage == 0 ? TotalItems / PerPage : TotalItems / PerPage + 1;
+        public bool CanNext => Page < TotalPages;
+        public bool CanPrevious => Page > 1;
+
+        public PagedListBase(int totalItems, PageInfo pageInfo)
+        {
+            if (totalItems < 0)
+            {
+                throw new ArgumentException($"{nameof(totalItems)} must be >= 0", nameof(totalItems));
+            }
+
+            TotalItems = totalItems;
+            Page = pageInfo.Page;
+            PerPage = pageInfo.PerPage;
+        }
+    }
+}
